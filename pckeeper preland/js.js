ec3352973130c9d5ea18a1b621651261877798bf
Luckyfoxdesign <sovenkovmaksim@gmail.com>
2017-08-03 14:15:01 +0300

    pc keeper added

---
+++ pckeeper preland/js.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  var date = new Date();
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var dd = date.getDate();
+
+  var mm = date.getMonth();
+
+  var yy = date.getFullYear();
+
+  var result = dd + " " + monthNames[mm] + " " + yy;
+
+  startTimer();
+
+  $("#data").text(result);
+  function startTimer(duration) {
+    var timer = 120,
+      minutes,
+      seconds;
+    setInterval(function() {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      $("#timer").text(minutes + " minutes " + seconds + " seconds ");
+
+      if (--timer < 0) {
+        timer = 120;
+      }
+    }, 1000);
+  }
+});
