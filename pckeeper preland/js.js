d2510444ac0aa7870610c9d10cda8075f37a49c5
Luckyfoxdesign <sovenkovmaksim@gmail.com>
2017-08-07 17:11:31 +0300

    on tab closed func added

--- pckeeper preland/js.js
+++ pckeeper preland/js.js
@@ -15,6 +15,17 @@ $(document).ready(function() {
     "December"
   ];
 
+  window.onbeforeunload = function (e) {
+      e = e || window.event;
+      // For IE and Firefox prior to version 4
+      if (e) {
+          e.returnValue = 'Sure?';
+      }
+
+      // For Safari
+      return 'Sure?';
+  };
+
   var dd = date.getDate();
 
   var mm = date.getMonth();
